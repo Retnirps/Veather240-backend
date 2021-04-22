253d5468745c182f92ad0af3fe0ba5548a95e967
Retnirps <44922573+Retnirps@users.noreply.github.com>
2021-04-22 13:18:06 +0300

    Add files via upload

--- js/router.js
+++ js/router.js
@@ -5,7 +5,7 @@ const CitySchema = require('../js/CitySchema');
 
 const router = Router();
 
-router.get(encodeURI("/weather/city"), asyncHandler(async (req, res) => {
+router.get("/weather/city", asyncHandler(async (req, res) => {
     res.send(await getWeatherByCityName(encodeURI(req.query.q)));
 }));
 
@@ -13,12 +13,12 @@ router.get("/weather/coordinates", asyncHandler(async (req, res) => {
     res.send(await getWeatherByCoordinates(req.query.lat, req.query.lon));
 }));
 
-router.get("/weather/favourites", asyncHandler(async (req, res) => {
+router.get("/favourites", asyncHandler(async (req, res) => {
     const citySchemas = await CitySchema.find({});
     res.send(citySchemas);
 }));
 
-router.post("/weather/favourites", asyncHandler(async (req, res) => {
+router.post("/favourites", asyncHandler(async (req, res) => {
     if (await isExist(req.query.city)) {
         res.statusCode = 400;
         res.send({ status: 'Bad Request', citySchema: 'already exists' });
@@ -37,7 +37,7 @@ router.post("/weather/favourites", asyncHandler(async (req, res) => {
     }
 }));
 
-router.delete("/weather/favourites", asyncHandler(async (req, res) => {
+router.delete("/favourites", asyncHandler(async (req, res) => {
     const cityFilter = req.query.city;
     await CitySchema.findOneAndDelete({city: cityFilter}).exec();
     return res.send({ status: 'OK', citySchema: 'deleted' });
